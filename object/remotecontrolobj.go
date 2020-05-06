baa7a52dc6e310d55b412b2532187c352b283980
wanghaichao <wanghaichao@jslcinfo.com>
2020-05-06 13:01:39 +0800

    modify

--- object/remotecontrolobj.go
+++ object/remotecontrolobj.go
@@ -1,6 +1,19 @@
 package object
 
+import (
+	"encoding/base64"
+	"encoding/json"
+)
+
 type RemoteControlObj struct {
 	ResponseType string
 	ResponseData interface{}
+}
+
+func (obj *RemoteControlObj) GetBase64String() (string, error) {
+	bytes, err := json.Marshal(*obj)
+	if err != nil {
+		return "", err
+	}
+	return base64.StdEncoding.EncodeToString(bytes), nil
 }
